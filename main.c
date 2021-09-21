70db63529d04f0e9090753a92e092d64aac6bddc
kenialo19 <3396@holbertonschool.com>
2021-09-21 17:14:11 -0500

    cearting main principal

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+/**
+ * main -Monty bytecodes files interpreter
+ * @argc: total of arguments
+ * @argv: The arguments, monty files
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+		error_argv();
+	op_re_file(argv);
+	return (0);
+}
